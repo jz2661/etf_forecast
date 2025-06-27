f456b6e8f73546b04cb76554fedc2f13c0629ea5
Jesse Zhang <Jessejz.Zhang@shangri-la.com>
2025-06-27 08:58:00 +0800

    qqq leverage

--- main_dnn_multi.py
+++ main_dnn_multi.py
@@ -7,13 +7,20 @@ def prob_to_dollar(p):
     plow = .58
     return max(0, (p-plow)/(pup-plow) * dup)
 
+def qqq_to_lever(pqqq):
+    return max(1, (pqqq-.5)/0.1)
+
 def run():
     df, last_row_sorted = predict_multi()
 
     maildf = last_row_sorted.to_frame()
     
     maildf.iloc[:, 0] = maildf.iloc[:, 0].round(3)
-    maildf['$size'] = maildf.iloc[:, 0].apply(prob_to_dollar).round().astype(int)
+    
+    maildf['$size'] = maildf.iloc[:, 0].apply(prob_to_dollar)
+    # QQQ leverage
+    maildf['$size'] *= maildf.iloc[:, 0]['QQQ']
+    maildf['$size'] = maildf['$size'].round().astype(int)
 
     print(maildf)
     send_mail(df=maildf, subject='DNN Multi Daily')
